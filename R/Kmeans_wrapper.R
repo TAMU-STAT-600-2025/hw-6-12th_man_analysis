df0ef8b7b4a830c04362196129f597f40226cce0
Caleb Kahan <ckahan@tamu.edu>
2025-11-14 13:27:54 -0600

    add documentation for Kmeans

--- R/Kmeans_wrapper.R
+++ R/Kmeans_wrapper.R
@@ -1,15 +1,20 @@
-#' Title
+#' K-Means Clustering
 #'
-#' @param X
-#' @param K
-#' @param M
-#' @param numIter
+#' @param X Matrix of data points, where rows are observations (n) and columns are features (p)
+#' @param K Integer specifying the number of clusters
+#' @param M An optional K x p matrix of initial cluster centroids. If NULL (default), K points are randomly sampled from X as initial centroids
+#' @param numIter The maximum number of iterations to run the algorithm (default: 100)
 #'
-#' @return Explain return
+#' @return Vector of length n containing the cluster assignments (integers from 1 to K) for each observation
 #' @export
 #'
 #' @examples
-#' # Give example
+#' X1 <- matrix(rnorm(50 * 2, mean = 0), 50, 2)
+#' X2 <- matrix(rnorm(50 * 2, mean = 4), 50, 2)
+#' data <- rbind(X1, X2)
+#'
+#' # Run K-Means with K=2
+#' cluster_assignments <- MyKmeans(data, K = 2, numIter = 100)
 MyKmeans <- function(X, K, M = NULL, numIter = 100) {
   n = nrow(X) # number of rows in X
   
