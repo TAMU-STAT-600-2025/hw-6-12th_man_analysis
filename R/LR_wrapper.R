24244f1a6c1cd8a32f092714014c5abcafd2781f
Caleb Kahan <ckahan@tamu.edu>
2025-11-13 21:57:24 -0600

    add compatibility checks to LRMultiClass

--- R/LR_wrapper.R
+++ R/LR_wrapper.R
@@ -15,7 +15,37 @@
 LRMultiClass <- function(X, y, beta_init = NULL, numIter = 50, eta = 0.1, lambda = 1){
   
   # Compatibility checks from HW3 and initialization of beta_init
-  
+  n <- nrow(X)
+  p <- ncol(X)
+  # Check that the first column of X and Xt are 1s, if not - display appropriate message and stop execution.
+  # Check that first column of X are 1s
+  if (!all(X[, 1] == 1)) {
+    stop("X should have an intercept column of only ones")
+  }
+  # Check that first column of Xt are 1s
+  if (!all(Xt[, 1] == 1)) {
+    stop("Xt should have an intercept column of only ones")
+  }
+  # Check for compatibility of dimensions between X and Y
+  if (n != length(y)) {
+    stop("X and Y should have same number of rows")
+  }
+  # Check for compatibility of dimensions between Xt and Yt
+  if (nrow(Xt) != length(yt)) {
+    stop("Xt and Yt should have same number of rows")
+  }
+  # Check for compatibility of dimensions between X and Xt
+  if (p != ncol(Xt)) {
+    stop("X and Xt should have same number of columns")
+  }
+  # Check eta is positive
+  if (eta <= 0) {
+    stop("eta should be positive")
+  }
+  # Check lambda is non-negative
+  if (lambda < 0) {
+    stop("lambda should be non-negative")
+  }
   
   # Call C++ LRMultiClass_c function to implement the algorithm
   out = LRMultiClass_c(X, y, beta_init, numIter, eta, lambda)
