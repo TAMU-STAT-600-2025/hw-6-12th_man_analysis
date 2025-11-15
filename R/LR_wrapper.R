16c6b01eb829e1f14e9841b195755fe2c0a31b3f
Mitchell Anderson <mitch7anderson@tamu.edu>
2025-11-14 19:19:03 -0600

    add documentation for multi-class logistic regression function

--- R/LR_wrapper.R
+++ R/LR_wrapper.R
@@ -1,17 +1,25 @@
-#' Title
+#' Multi-Class Logistic Regression
 #'
-#' @param X 
-#' @param y 
-#' @param numIter 
-#' @param eta 
-#' @param lambda 
-#' @param beta_init 
+#' @param X n x p training data, 1st column should be 1s to account for intercept
+#' @param y a vector of size n of class labels, from 0 to K-1
+#' @param numIter a number of fixed iterations for the algorithm, default value is 50
+#' @param eta learning rate, default value is 0.1
+#' @param lambda ridge parameter, default value is 1
+#' @param beta_init (optional) initial starting values of beta for the algorithm, should be p x K matrix
 #'
-#' @return
+#' @return p X K matrix of estimated beta values after numIter iterations
 #' @export
 #'
 #' @examples
-#' # Give example
+#' set.seed(123)
+#' n <- 12
+#' p <- 3
+#' K <- 3
+#' X_1 <- rnorm(n, mean = 0, sd = 1)
+#' X_2 <- rnorm(n, mean = 0, sd = 0.5)
+#' X <- cbind(1, X_1, X_2)
+#' y <- sample(0:(K-1), n, replace = TRUE)
+#' beta_hat <- LRMultiClass(X, y)
 LRMultiClass <- function(X, y, beta_init = NULL, numIter = 50, eta = 0.1, lambda = 1){
   
   # Compatibility checks from HW3 and initialization of beta_init
